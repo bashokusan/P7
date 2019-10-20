3306073b215fd52e6a48ba3f300e6d4134110945
bashokusan <pierresimonnet1@gmail.com>
2019-10-20 23:01:32 +0200

    create one entity and its controller to check if setup/config works fine

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
+use App\Entity\Product;
+
+class ProductController extends AbstractFOSRestController
+{
+    /**
+    * @Get(
+    *      path = "/items",
+    *      name = "app_items_list",
+    * )
+    * @View()
+    */
+    public function showAll()
+    {
+        $product = new Product();
+        $product->setName('nom du produit');
+        return $product;
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ * @ExclusionPolicy("all")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     *
+     * @Expose
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Expose
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
