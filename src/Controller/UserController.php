7a21e71c458095258da8c2915befbc587ea6e85f
bashokusan <pierresimonnet1@gmail.com>
2019-10-25 13:06:46 +0200

    creation des routes

---
+++ src/Controller/UserController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\Annotations\Delete;
+use FOS\RestBundle\Controller\Annotations\Put;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use App\Exception\ResourceViolationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use FOS\RestBundle\Request\ParamFetcher;
+use App\Entity\ProductUser;
+
+class UserController extends AbstractFOSRestController
+{
+    /**
+    * @Get(
+    *      path = "/users",
+    *      name = "app_users_list",
+    * )
+    * @View()
+    */
+    public function showAll()
+    {
+        $users= $this->getDoctrine()->getRepository(ProductUser::class)->findAll();
+        return $users;
+    }
+
+    /**
+    * @Get(
+    *      path = "/users/{id}",
+    *      name = "app_users_show",
+    *      requirements = {"id"="\d+"}
+    * )
+    * @View()
+    */
+    public function showAction(ProductUser $user)
+    {
+        return $user;
+    }
+
+    /**
+     * @Post(
+     *    path = "/users",
+     *    name = "app_user_create"
+     * )
+     * @ParamConverter("user", converter="fos_rest.request_body")
+     * @param ProductUser $user
+     * @param ConstraintViolationListInterface $validationErrors
+     * @return \FOS\RestBundle\View\View
+     * @throws ResourceViolationException
+     */
+    public function createAction(ProductUser $user, ConstraintViolationListInterface $validationErrors)
+    {
+        //$user->setClient($this->getUser());
+
+        if(count($validationErrors) > 0){
+            //return $this->view($validationErrors, Response::HTTP_BAD_REQUEST);
+            $message = 'The JSON sent contains invalid data. Here are the errors you need to correct: ';
+            foreach ($validationErrors as $violation) {
+                $message .= sprintf("Field %s: %s ", $violation->getPropertyPath(), $violation->getMessage());
+            }
+            throw new ResourceViolationException($message);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $this->view(
+            $user,
+            Response::HTTP_CREATED,
+            ['Location' => $this->generateUrl('app_users_show', ['id' => $user->getId(), UrlGeneratorInterface::ABSOLUTE_URL])]);
+    }
+
+    /**
+     * @Put(
+     *    path = "/users/{id}",
+     *    name = "app_user_update"
+     * )
+     * @ParamConverter("newUser", converter="fos_rest.request_body")
+     */
+    public function updateAction(ProductUser $user, ProductUser $newUser, ConstraintViolationListInterface $validationErrors)
+    {
+        if(count($validationErrors) > 0){
+            //return $this->view($validationErrors, Response::HTTP_BAD_REQUEST);
+            $message = 'The JSON sent contains invalid data. Here are the errors you need to correct: ';
+            foreach ($validationErrors as $violation) {
+                $message .= sprintf("Field %s: %s ", $violation->getPropertyPath(), $violation->getMessage());
+            }
+            throw new ResourceViolationException($message);
+
+        }
+
+        $user->setName($newUser->getName());
+        $user->setEmail($newUser->getEmail());
+
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+
+        return $this->view(
+            $user,
+            Response::HTTP_CREATED,
+            ['Location' => $this->generateUrl('app_users_show', ['id' => $user->getId(), UrlGeneratorInterface::ABSOLUTE_URL])]);
+    }
+
+    /**
+     * @Delete(
+     *    path = "/users/{id}",
+     *    name = "app_user_delete",
+     *    requirements = {"id"="\d+"}
+     * )
+     * @View(StatusCode = 204)
+     */
+    public function deleteAction(ProductUser $user)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        return;
+    }
+}
--- src/Entity/ProductUser.php
+++ src/Entity/ProductUser.php
@@ -3,26 +3,74 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
+use Hateoas\Configuration\Annotation as Hateoas;
 
-/**
- * @ORM\Entity(repositoryClass="App\Repository\ProductUserRepository")
- */
+  /**
+  * @ORM\Entity(repositoryClass="App\Repository\ProductUserRepository")
+  * @UniqueEntity(fields={"email"}, message="Cet utilisateur existe déjà")
+  *
+  * @ExclusionPolicy("all")
+  *
+  * @Hateoas\Relation(
+  *    "self",
+  *    href = @Hateoas\Route(
+  *        "app_users_show",
+  *        parameters = {"id" = "expr(object.getId())"},
+  *        absolute = true
+  *    )
+  * )
+  *
+  * @Hateoas\Relation(
+  *    "update",
+  *    href = @Hateoas\Route(
+  *        "app_user_update",
+  *        parameters = {"id" = "expr(object.getId())"},
+  *        absolute = true
+  *    )
+  * )
+  *
+  * @Hateoas\Relation(
+  *    "delete",
+  *    href = @Hateoas\Route(
+  *        "app_user_delete",
+  *        parameters = {"id" = "expr(object.getId())"},
+  *        absolute = true
+  *    )
+  * )
+  *
+  * @Hateoas\Relation(
+  *    "client",
+  *    embedded = @Hateoas\Embedded("expr(object.getclient())")
+  * )
+  */
 class ProductUser
 {
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     *
+     * @Expose
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     *
+     * @Expose
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     *
+     * @Expose
      */
     private $email;
 
